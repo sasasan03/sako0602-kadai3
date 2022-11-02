bab0985d6e7ccf6b7c26eab5182393f242b499b9
sasasan03 <sako.hiro62@gmail.com>
2022-11-02 20:47:25 +0900

    課題３の提出

--- sako0602-kadai3/ContentView.swift
+++ sako0602-kadai3/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var numText1 = ""
+    @State var numText2 = ""
+    @State var flag1 = true
+    @State var flag2 = true
+    @State var updateNum1 = 0
+    @State var updateNum2 = 0
+    @State var total = 0
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                VStack{
+                    TextField("", text: $numText1)
+                        .frame(width: 70,height: 50)
+                        .border(Color.black)
+                        .padding()
+                    HStack{
+                        Text("+")
+                        Toggle("", isOn: $flag1)
+                        Text("-")
+                    }
+                    .fixedSize()
+                }
+                VStack{
+                    TextField("", text: $numText2)
+                        .frame(width: 70,height: 50)
+                        .border(Color.black)
+                        .padding()
+                    HStack{
+                        Text("+")
+                        Toggle("", isOn: $flag2)
+                        Text("-")
+                    }
+                    .fixedSize()
+                }
+            }
+            .padding()
+            Button {
+                calculation()
+            } label: {
+                Text("Button")
+            }
+            .padding()
+            HStack{
+                Spacer()
+                Text("\(updateNum1)")
+                Spacer()
+                Text("\(updateNum2)")
+                Spacer()
+            }
+            .padding()
+            Text("\(total)")
         }
-        .padding()
+    }
+    func calculation() {
+        let num1 = (Int(numText1)) ?? 0
+        let num2 = (Int(numText2)) ?? 0
+       
+        updateNum1 = flag1 ? -num1 : num1
+        updateNum2 = flag2 ? -num2 : num2
+        
+        total = updateNum1 + updateNum2
     }
 }
 
--- sako0602-kadai3/ExperimentView.swift
+++ sako0602-kadai3/ExperimentView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct ExperimentView: View {
+    @State var numText1 = ""
+    @State var total = 0
+    @State var inPutNum1 = 0
+    @State var flag = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            TextField("aaa", text: $numText1)
+            Toggle("" ,isOn: $flag)
+            Button {
+               
+            } label: {
+                Text("Button")
+            }
+                Text("\(inPutNum1)")
+            Text("答えだよん\(total)")
+        }
+    }
+    func calc() {
+        let num1 = (Int(numText1)) ?? 0
+        
+        inPutNum1 = flag ? num1 : -num1
+        
+        total = inPutNum1
     }
 }
 
